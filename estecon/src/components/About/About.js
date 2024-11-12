de2d4ec1c15335bf2c0d2f807e5fa3921a252a91
kelySoare5 <kelysoares688@gmail.com>
2024-11-11 20:21:01 -0500

    styled: image responsiveness

--- estecon/src/components/About/About.js
+++ estecon/src/components/About/About.js
@@ -20,19 +20,19 @@ export const About = () => {
             </div>
 
             <div className=
-            "flex space-x-96 my-7 max-md:block max-md:space-x-0"
+            "flex space-x-72 my-7 max-2xl:block max-2xl:space-x-0"
             >
                 <img src={about} alt="Foto de 1975 " className=
-                "w-2/6 max-md:w-3/5 max-md:m-auto"
+                "w-2/5 max-2xl:w-2/5 max-2xl:m-auto"
                 />
 
                 <div className=
-                "w-2/5 leading-7 text-justify text-2xl max-md:pt-5 max-md:w-auto max-md:text-base"
+                "w-2/5 leading-7 text-justify text-2xl max-2xl:pt-5 max-2xl:w-auto max-md:text-base"
                 >
                     <p>
                     Fundada em 1975, a Estecon é uma empresa especializada em serviços contábeis, comprometida em oferecer soluções completas e personalizadas para empresas de diversos portes e setores. Atuando com profissionalismo e dedicação, nossa equipe de especialistas está preparada para cuidar de todas as demandas contábeis, fiscais e tributárias, garantindo segurança, agilidade e conformidade com a legislação.
                     </p>
-                    <p className="mt-10 max-md:mt-3">Em 2023, realizamos uma ampliação e reforma em nossas instalações, aprimorando ainda mais nossa estrutura para melhor atender nossos clientes. Na Estecon, buscamos ser parceiros estratégicos, contribuindo para o desenvolvimento e o sucesso sustentável dos negócios de nossos clientes.</p>
+                    <p className="mt-10 max-2xl:mt-3">Em 2023, realizamos uma ampliação e reforma em nossas instalações, aprimorando ainda mais nossa estrutura para melhor atender nossos clientes. Na Estecon, buscamos ser parceiros estratégicos, contribuindo para o desenvolvimento e o sucesso sustentável dos negócios de nossos clientes.</p>
                 </div>
 
             </div>
