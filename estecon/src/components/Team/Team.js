b1f99065571ee8f56249507dba2e4bc981e3ef26
brunolimapinheiro <brunol21082001@gmail.com>
2024-12-02 15:48:03 -0300

    adicionando a parte do time

--- estecon/src/components/Team/Team.js
+++ estecon/src/components/Team/Team.js
@@ -6,55 +6,62 @@ import img5 from "../../assets/img/team/amelia.jpeg"
 import img6 from "../../assets/img/team/daylan.jpeg"
 
 
+
 export const Team = ()=>{
     return(
         <>
-            <section className="w-full">
-                <div className="text-center">
-                    <h1 className="text-bluePrimary font-bold text-4xl py-1 text-center">Equipe</h1>
-                    <hr className="bg-bluePrimary  p-1 w-96 mx-auto max-md:w-64"/>
-                    <p className="text-bluePrimary py-1 font-bold text-center text-3xl max-md:text-xl">Conheça nossos profissionais</p>
-                    
-                    <div className="flex items-center justify-center">
-                    <div className="grid grid-cols-4 grid-rows-2 gap-4">
-                        <div className="text-center">
-                            <img src={img1} alt="imagem" className="w-90 h-90 " />
-                            <h1 className="text-bluePrimary font-bold text-4xl py-1 text-center ">Sergino Castro</h1>
-                        </div>
-                        <div className="text-center">
-                            <img src={img1} alt="imagem"className="w-90 h-90" />
-                            <h1 className="text-bluePrimary font-bold text-4xl py-1 text-center">Cythia Castro</h1>
-                        </div>
-                        <div className="text-center">
-                            <img src={img1} alt="imagem" className="w-90 h-90" />
-                            <h1 className="text-bluePrimary font-bold text-4xl py-1 text-center">Clistenes Castro</h1>
-                        </div>
+          <section className="w-full text-center"> 
+            <div className="text-center"> 
+                <h1 className="text-bluePrimary font-bold text-4xl py-1 text-center">Equipe</h1> 
+                <hr className="bg-bluePrimary p-1 w-96 mx-auto max-md:w-64"/> 
+                <p className="text-bluePrimary py-1 font-bold text-center text-3xl max-md:text-xl">Conheça nossos profissionais</p> 
+                <div className="grid grid-cols-2 md:grid-cols-4 sm:grid-cols-2 sm:grid-rows-2 gap-4 p-20"> 
+                    <div className="text-center"> 
+                        <img src={img1} alt="imagem" className=" lg:w-80 lg:h-80 sm:w-20 sm:h-20 rounded-full mx-auto " /> 
+                    <h1 className="text-bluePrimary font-bold text-4xl py-1">Sergino Castro</h1> 
+                    <p>Fundador e líder da Estecon
+                    sempre comprometido com a integridade e o sucesso dos clientes</p>
+                    </div> 
+                    <div className="text-center"> 
+                        <img src={img1} alt="imagem" className="lg:w-80 lg:h-80 sm:w-30 sm:h-20 w-25 rounded-full mx-auto" />
+                     <h1 className="text-bluePrimary font-bold text-4xl py-1">Cythia Castro</h1>
+                     <p>Atuante na parte da gerência da empresa garantindo a eficiência aos processos dos clientes</p>
+                     </div> 
+                     <div className="text-center"> 
+                        <img src={img1} alt="imagem" className="lg:w-80 lg:h-80 sm:w-30 sm:h-20 w-25 rounded-full mx-auto" /> 
+                        <h1 className="text-bluePrimary font-bold text-4xl py-1">Clistenes Castro</h1>
+                        <p>Atua na parte de TI , Agente de Registro da Certificadora Online e representante da Toronto</p>
+                        </div> 
                         <div className="text-center"> 
+                            <img src={img2} alt="imagem" className="lg:w-80 lg:h-80 sm:w-30 sm:h-20 w-25 rounded-full mx-auto" />
+                         <h1 className="text-bluePrimary font-bold text-4xl py-1">Mikaele Ravena</h1>
+                         <p> Trabalha na parte de atendimento ao cliente e responsável pelas empresas</p>
+                         </div> 
+                         <div className="text-center"> 
+                            <img src={img3} alt="imagem" className="lg:w-80 lg:h-80 sm:w-30 sm:h-20 w-25 rounded-full mx-auto" /> 
+                         <h1 className="text-bluePrimary font-bold text-4xl py-1">Kelson Adriano</h1>
+                         <p> Trabalha na parte de atendimento ao cliente e responsável pelas empresas,</p>
+                         </div> 
+                         <div className="text-center"> 
+                            <img src={img4} alt="imagem" className="lg:w-80 lg:h-80 sm:w-30 sm:h-20 w-25 rounded-full mx-auto" /> 
+                            <h1 className="text-bluePrimary font-bold text-4xl py-1">Carlos Sousa</h1>
+                            <p>Atuante na parte de TI, dando suporte a clientes que usam o programa de nota fiscal Toronto</p>
+                            <p></p>
+                            </div> 
+                            <div className="text-center">
+                                 <img src={img5} alt="imagem" className="lg:w-80 lg:h-80 sm:w-30 sm:h-20 w-25 rounded-full mx-auto" />
+                                  <h1 className="text-bluePrimary font-bold text-4xl py-1">Amélia Alves</h1> 
+                                  <p> Trabalha na parte de atendimento ao cliente e responsável pelas empresas,</p>
+                                  </div> 
+                            <div className="text-center"> 
+                                <img src={img6} alt="imagem" className="lg:w-80 lg:h-80 sm:w-30 sm:h-20 w-25 rounded-full mx-auto" /> 
+                                  <h1 className="text-bluePrimary font-bold text-4xl py-1">Daylan Alves</h1>
+                                  <p> Responsável pela entrega de impostos - Apresente sua experiência na área fiscal.</p>
+                                  </div> 
+                                  </div> 
+                                  </div> 
+                                  </section>
 
-                            <img src={img2} alt="imagem" className="w-90 h-90" />
-
-                            <h1 className="text-bluePrimary font-bold text-4xl py-1 text-center">Mikaele Ravena</h1>
-                        </div>
-                        <div className="text-center">
-                            <img src={img3} alt="imagem" className="w-90 h-90" />
-                            <h1 className="text-bluePrimary font-bold text-4xl py-1 text-center">Kelson Adriano</h1>
-                        </div>
-                        <div className="text-center">
-                            <img src={img4} alt="imagem" className="w-90 h-90" />
-                            <h1 className="text-bluePrimary font-bold text-4xl py-1 text-center">Carlos Sousa</h1>
-                        </div>
-                        <div className="text-center">
-                            <img src={img5} alt="imagem" className="w-90 h-90" />
-                            <h1 className="text-bluePrimary font-bold text-4xl py-1 text-center">Amélia ALves</h1>
-                        </div>
-                        <div className="text-center">
-                            <img src={img6} alt="imagem" className="w-90 h-90" />
-                            <h1 className="text-bluePrimary font-bold text-4xl py-1 text-center">Daylan Alves</h1>
-                        </div>
-                    </div>
-                 </div>
-                </div>
-            </section>
         </>
     )
 }
