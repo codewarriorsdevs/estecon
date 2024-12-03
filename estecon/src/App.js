19add032ca61ecdffd945e9471c28fbf19b12726
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2024-12-03 10:44:33 -0300

    feat: New Local Section

--- estecon/src/App.js
+++ estecon/src/App.js
@@ -1,5 +1,6 @@
 import { About } from "./components/About/About";
-import Navbar from "./components/Navbar/Navbar";
+import { Local } from "./components/Local/Local";
+import { Navbar } from "./components/Navbar/Navbar";
 import { Segments } from "./components/Segments/Segments";
 import { ServicesSec } from "./components/ServicesSec/ServicesSec";
 import { Welcome } from "./components/Welcome/Welcome";
@@ -7,13 +8,12 @@ import { Welcome } from "./components/Welcome/Welcome";
 function App() {
   return (
     <>
-
       <Navbar />
       <Welcome />
       <About />
       <ServicesSec />
       <Segments />
-
+      <Local />
     </>
   );
 }
